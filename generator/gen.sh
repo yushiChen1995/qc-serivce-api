9457cf0adfb2d0b23f78620512e663686419f735
chenyushi <chenyushi@lechebang.com>
2019-08-21 14:26:49 +0800

    青橙-接口及实体类服务

---
+++ generator/gen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar mybatis-generator-core-1.3.4.jar -configfile generatorConfig_$1.xml -overwrite
---
+++ src/main/java/com/qc/service/IBrandService.java
@@ -0,0 +1,13 @@
+package com.qc.service;
+
+import com.qc.pojo.goods.Brand;
+
+import java.util.List;
+
+/**
+ * @author cys
+ * @date 2019/8/21
+ */
+public interface IBrandService {
+    List<Brand> getAllBrand();
+}
